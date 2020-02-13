7411d378ec212ee8bcd796e4ae588271b094ddc4
Jamie Quinn <j.quinn.1@research.gla.ac.uk>
2020-02-13 14:00:06 +0000

    Refactored analysis notebooks into three categories - single case, parameter study and misc

---
+++ images/kink_instability/plotting_parameters.py
@@ -0,0 +1,7 @@
+PAD_INCHES = 0.02
+main_data_folder = "/nas/1101974q/archie-latest-organisation-sept-2018/kink-instabilities/initially-unstable/"
+
+FONTSIZE = 7
+COLUMN_WIDTH = 468.0 * 1/72.27
+COLUMN_HALFSIZE = 0.5 # Multiplier
+COLUMN_THIRDSIZE = 0.32
