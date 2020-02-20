ea0262d196f12d58eb87a088f695557583e5067a
Jamie Quinn <j.quinn.1@research.gla.ac.uk>
2020-02-20 00:50:16 +0000

    Added field line plots

--- images/kink_instability/field_line_plots/fill_filelist.sh
+++ images/kink_instability/field_line_plots/fill_filelist.sh
@@ -3,7 +3,8 @@ ISOTROPIC_DIR=/nas/1101974q/archie-latest-organisation-sept-2018/kink-instabilit
 SWITCHING_DIR=/nas/1101974q/archie-latest-organisation-sept-2018/kink-instabilities/initially-unstable/cadence-5-v1e-4r5e-4.5/v1e-4r5e-4.5-switching
 #ls ${ISOTROPIC_DIR}/Data/*.sdf ${SWITCHING_DIR}/Data/*.sdf | sed -e 's/ /\\n/g' > filename_list
 
-echo ${ISOTROPIC_DIR}/Data/0009.sdf | sed -e 's/ /\\n/g' > filename_list
+echo ${ISOTROPIC_DIR}/Data/0000.sdf | sed -e 's/ /\\n/g' > filename_list
+echo ${ISOTROPIC_DIR}/Data/0009.sdf | sed -e 's/ /\\n/g' >> filename_list
 echo ${ISOTROPIC_DIR}/Data/0010.sdf | sed -e 's/ /\\n/g' >> filename_list
 echo ${ISOTROPIC_DIR}/Data/0035.sdf | sed -e 's/ /\\n/g' >> filename_list
 echo ${SWITCHING_DIR}/Data/0035.sdf | sed -e 's/ /\\n/g' >> filename_list
