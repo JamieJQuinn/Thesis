0bb567060c2f1c864a95988f3cd279a56386c920
Jamie Quinn <j.quinn.1@research.gla.ac.uk>
2020-04-09 14:18:58 +0100

    Refactored analysis functions

--- images/visualisation-tools/slice_helper_functions.py
+++ images/visualisation-tools/slice_helper_functions.py
@@ -42,6 +42,16 @@ def get_dx_dy_dz(sdfFile, cell_centre=False):
 def get_variable(sdfFile, varname):
     return getattr(sdfFile, varname)
 
+def get_magnetic_field(sdfFilename):
+    sdfFile = sdf.read(sdfFilename)
+
+    mag_field = np.array([
+        getattr(sdfFile, variable).data for variable in
+        ['Magnetic_Field_bx_centred', 'Magnetic_Field_by_centred', 'Magnetic_Field_bz_centred']
+    ])
+
+    return mag_field
+
 def plot_slice(sdfFilename, variable_name, dimension, slice_loc,
                cbar=INCLUDE_CBARS,
                include_title=INCLUDE_TITLE,
@@ -243,84 +253,3 @@ def dimensionalise_temperature(tempIn):
     T0 = (B0*B0)*mf*mh_si/(kb_si*mu0_si*RHO0) * 1e9
 
     return T0*tempIn
-
-def plot_current_density_and_velocity(sdfFile, outname, xLimits=(-2, 2), yLimits=(-2, 2),\
-                                      max_current=-1, max_temperature=-1, print_maxes=False,\
-                                     remove_axes=False):
-    latexify(columns=2, square=True)
-    fig,axis = plt.subplots()
-
-    # if not remove_axes:
-        # # Make room for labels
-        # fig.subplots_adjust(left=.12, bottom=0.05, right=.965, top=.99)
-    # else:
-        # fig.subplots_adjust(left=0, bottom=0, right=1, top=1)
-
-    xIndexLimits = tuple((length_to_index(length) for length in xLimits))
-    yIndexLimits = tuple((length_to_index(length) for length in yLimits))
-    
-    current_density = get_magnitude_current_at(sdfFile, 350, xLimits=xIndexLimits, yLimits=yIndexLimits)
-    if max_current < 0:
-        max_current = int(current_density.max())
-    
-    axis.imshow(current_density,\
-                extent=[xLimits[0], xLimits[1], yLimits[0], yLimits[1]],\
-                vmax=max_current, vmin=0,\
-                interpolation='bilinear',\
-                cmap=plt.get_cmap("Blues"))
-    if print_maxes:
-        axis.text(-1, 0.9, "Max Current: " + str(max_current), color='w')
-    
-    print("Max current:", max_current)
-    
-    temperature = get_temperature_at(sdfFile, 350, xLimits=xIndexLimits, yLimits=yIndexLimits)
-    print(dimensionalise_temperature(temperature[0,0]))
-    if max_temperature < 0:
-        max_temperature = int(dimensionalise_temperature(temperature.max()))
-    
-    axis.imshow(dimensionalise_temperature(temperature),\
-                extent=[xLimits[0], xLimits[1], -yLimits[1], yLimits[0]],\
-                vmax=max_temperature, vmin=2e4,\
-                interpolation='bilinear',\
-                cmap=plt.get_cmap("Reds"))
-    
-    if print_maxes:
-        axis.text(-1, -0.95, "Max temp: " + str(int(max_temperature/1e6)), color='w')
-    
-    print(r"Max temperature $\times 10^{-6}$:", max_temperature/1e6)
-            
-    vx = slice_variable(\
-        get_variable(sdfFile, "Velocity_Vx"),\
-        z_min=350, z_max = 350 + 1)
-    vy = slice_variable(\
-        get_variable(sdfFile, "Velocity_Vy"),\
-        z_min=350, z_max = 350 + 1)
-    
-    vx = 0.25*(vx[1:,1:] + vx[:-1,1:] + vx[:-1,:-1] + vx[1:,:-1])
-    vy = 0.25*(vy[1:,1:] + vy[:-1,1:] + vy[:-1,:-1] + vy[1:,:-1])
-    
-    vectorIdx = 6
-    
-    vx = vx.squeeze(axis=2)[\
-                            length_to_index(xLimits[0])+int(vectorIdx/2):length_to_index(xLimits[1]):vectorIdx,\
-                            length_to_index(yLimits[1])-int(vectorIdx/2):length_to_index(yLimits[0]):-vectorIdx\
-                           ].transpose()
-    vy = -vy.squeeze(axis=2)[\
-                            length_to_index(xLimits[0])+int(vectorIdx/2):length_to_index(xLimits[1]):vectorIdx,\
-                            length_to_index(yLimits[1])-int(vectorIdx/2):length_to_index(yLimits[0]):-vectorIdx\
-                           ].transpose()
-    
-    X, Y = np.meshgrid(\
-                      np.linspace(xLimits[0], xLimits[1], vx.shape[1]),\
-                      np.linspace(-yLimits[1], yLimits[0], vx.shape[0])\
-                      )
-    
-    if remove_axes:
-        axis.tick_params(axis='both', which='both', bottom=False, top=False, labelbottom=False, right=False, left=False, labelleft=False)
-
-#     axis.streamplot(X, Y, vx, vy)
-    axis.quiver(X, Y, vx, vy, units='xy', pivot='tail', width=0.007, color='k')
-    
-    save_plot(outname)
-    plt.show()
-    plt.close(fig)
